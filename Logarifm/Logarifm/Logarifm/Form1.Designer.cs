ebad44977430b2c8d0272806e71a5e0a48a62d54
Melques <57859163+Melques@users.noreply.github.com>
2023-04-09 01:51:09 +0300

    Add files via upload
    Формула из текстбокса преобразуется в функцию f(double x), которая возвращаетзначение введённой функции в иксе.  Преобразование происходи в фунции FormulaPrep после нажатия на кнопку btnFormula ("Вычислить").

---
+++ Logarifm/Logarifm/Logarifm/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Logarifm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_sim = new System.Windows.Forms.Button();
+            this.btn_sqr = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.textSim = new System.Windows.Forms.TextBox();
+            this.text_sqr = new System.Windows.Forms.TextBox();
+            this.text_del = new System.Windows.Forms.TextBox();
+            this.FormulaTextBox = new System.Windows.Forms.TextBox();
+            this.btnFormula = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.ArgTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_sim
+            // 
+            this.btn_sim.Location = new System.Drawing.Point(464, 220);
+            this.btn_sim.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_sim.Name = "btn_sim";
+            this.btn_sim.Size = new System.Drawing.Size(100, 28);
+            this.btn_sim.TabIndex = 0;
+            this.btn_sim.Text = "Simpson";
+            this.btn_sim.UseVisualStyleBackColor = true;
+            this.btn_sim.Click += new System.EventHandler(this.btn_sim_Click);
+            // 
+            // btn_sqr
+            // 
+            this.btn_sqr.Location = new System.Drawing.Point(464, 256);
+            this.btn_sqr.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_sqr.Name = "btn_sqr";
+            this.btn_sqr.Size = new System.Drawing.Size(100, 28);
+            this.btn_sqr.TabIndex = 2;
+            this.btn_sqr.Text = "Sqr";
+            this.btn_sqr.UseVisualStyleBackColor = true;
+            this.btn_sqr.Click += new System.EventHandler(this.btn_sqr_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(464, 292);
+            this.btn_del.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(100, 28);
+            this.btn_del.TabIndex = 3;
+            this.btn_del.Text = "Del";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // textSim
+            // 
+            this.textSim.Location = new System.Drawing.Point(572, 220);
+            this.textSim.Margin = new System.Windows.Forms.Padding(4);
+            this.textSim.Name = "textSim";
+            this.textSim.Size = new System.Drawing.Size(132, 22);
+            this.textSim.TabIndex = 4;
+            this.textSim.TextChanged += new System.EventHandler(this.textSim_TextChanged);
+            // 
+            // text_sqr
+            // 
+            this.text_sqr.Location = new System.Drawing.Point(572, 256);
+            this.text_sqr.Margin = new System.Windows.Forms.Padding(4);
+            this.text_sqr.Name = "text_sqr";
+            this.text_sqr.Size = new System.Drawing.Size(132, 22);
+            this.text_sqr.TabIndex = 5;
+            this.text_sqr.TextChanged += new System.EventHandler(this.text_sqr_TextChanged);
+            // 
+            // text_del
+            // 
+            this.text_del.Location = new System.Drawing.Point(572, 295);
+            this.text_del.Margin = new System.Windows.Forms.Padding(4);
+            this.text_del.Name = "text_del";
+            this.text_del.Size = new System.Drawing.Size(132, 22);
+            this.text_del.TabIndex = 6;
+            this.text_del.TextChanged += new System.EventHandler(this.text_del_TextChanged);
+            // 
+            // FormulaTextBox
+            // 
+            this.FormulaTextBox.Location = new System.Drawing.Point(54, 67);
+            this.FormulaTextBox.Name = "FormulaTextBox";
+            this.FormulaTextBox.Size = new System.Drawing.Size(328, 22);
+            this.FormulaTextBox.TabIndex = 7;
+            // 
+            // btnFormula
+            // 
+            this.btnFormula.Location = new System.Drawing.Point(388, 30);
+            this.btnFormula.Name = "btnFormula";
+            this.btnFormula.Size = new System.Drawing.Size(176, 79);
+            this.btnFormula.TabIndex = 8;
+            this.btnFormula.Text = "Вычислить";
+            this.btnFormula.UseVisualStyleBackColor = true;
+            this.btnFormula.Click += new System.EventHandler(this.btnFormula_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(134, 105);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(197, 22);
+            this.resultTextBox.TabIndex = 9;
+            // 
+            // ArgTextBox
+            // 
+            this.ArgTextBox.Location = new System.Drawing.Point(282, 36);
+            this.ArgTextBox.Name = "ArgTextBox";
+            this.ArgTextBox.Size = new System.Drawing.Size(82, 22);
+            this.ArgTextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "x =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "f(x) =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "значение в иксе";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ArgTextBox);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.btnFormula);
+            this.Controls.Add(this.FormulaTextBox);
+            this.Controls.Add(this.text_del);
+            this.Controls.Add(this.text_sqr);
+            this.Controls.Add(this.textSim);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_sqr);
+            this.Controls.Add(this.btn_sim);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sim;
+        private System.Windows.Forms.Button btn_sqr;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.TextBox textSim;
+        private System.Windows.Forms.TextBox text_sqr;
+        private System.Windows.Forms.TextBox text_del;
+        private System.Windows.Forms.TextBox FormulaTextBox;
+        private System.Windows.Forms.Button btnFormula;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.TextBox ArgTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Logarifm/Logarifm/Logarifm/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+
+using System.Text.RegularExpressions;
+
+namespace Logarifm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static void FormulaPrep(ref string str)
+        {
+            str = str.Replace('^', ',');
+            Stack<int> brackets = new Stack<int>();
+            int lstNumStart = 0;
+            int endIndex = str.Length;
+            int i = 0;
+            char curChar;
+            bool isPow = false;
+            bool isBracketsPow = false;
+            while (true)
+            {
+                if (i >= endIndex)
+                {
+                    if (isPow) { str = str.Insert(endIndex, ")"); }
+                    break;
+                }
+                curChar = str[i];
+                if(str[i] == ',')
+                {
+                    if (brackets.Count() > 0 && isBracketsPow)
+                    {
+                        str = str.Insert(brackets.Pop(), "pow(");
+                        isBracketsPow = false;
+                    }
+                    else {
+                        str = str.Insert(lstNumStart, "pow(");
+                    }
+                    endIndex = endIndex + 4;
+                    isPow = true;
+                    i = i + 6;
+                    continue;
+
+                }
+                if (curChar == '(') {
+                    brackets.Push(i); }
+                if (curChar == ')')  {
+                    if (i < endIndex - 1 && str[i + 1] == ',') {
+                        isBracketsPow = true;
+                    }
+                    else brackets.Pop(); }
+                if (!char.IsDigit(str[i]) &&  str[i] != 'x' && str[i] != 'X')
+                {
+                    lstNumStart = i + 1;
+                    if (isPow)
+                    {
+                        str = str.Insert(i, ")");
+                        isPow = false;
+                        i = i+2;
+                        endIndex++;
+                        continue;
+                    }
+                }
+                i++;
+            }
+        }
+
+       
+        private static Formula CreateFormula(string str)
+        {
+            string src =
+            @"using System;
+ 
+         static class Code
+         {
+             public static double Formula(double x, double y, double z)
+             {
+                 return {source};
+             }
+         }";
+            str = Regex.Replace(str.ToLower(), @"[a-z]{3}", x =>
+            {
+                if (x.Value == "sin") return "Math.Sin";
+                if (x.Value == "cos") return "Math.Cos";
+                if (x.Value == "log") return "Math.Log";
+                if (x.Value == "pow") return "Math.Pow";
+                if (x.Value == "ppi") return "Math.PI";
+                return x.Value;
+            });
+            src = src.Replace("{source}", str);
+            var compiler = CodeDomProvider.CreateProvider("C#");
+            var result = compiler.CompileAssemblyFromSource(new CompilerParameters(), src);
+            if (result.Errors.Count == 0)
+            {
+                var assembly = result.CompiledAssembly;
+                var type = assembly.GetType("Code");
+                var method = type.GetMethod("Formula");
+                return (Formula)Delegate.CreateDelegate(typeof(Formula), method);
+            }
+            return null;
+        }
+
+        public delegate double Formula(double x, double y, double z);
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            /*
+class Program
+{
+    
+    static double Abs(double x)
+    {
+        return (x < 0) ? -x : x;
+    }
+
+    
+    static int Sign(double x)
+    {
+        return (x < 0) ? -1 : 1;
+    }
+
+    
+    static double BisectionMethod(Func<double, double> function, double a, double b, double precision = 1e-10)
+    {
+        while (true)
+        {
+            var t = (a + b) / 2;
+
+            if (function(t) == 0.0 || Abs(b - a) < Abs(precision))
+            {
+                return t;
+            }
+
+            if (Sign(function(t)) == Sign(function(a)))
+            {
+                a = t;
+            }
+            else
+            {
+                b = t;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        
+        double f(double x) => 3 * Math.Pow(x, 2) - 6 * x + 1;
+
+        Console.WriteLine("3x^2 - 6x + 1");
+        Console.Write("Введите начало отрезка: ");
+        var a = double.Parse(Console.ReadLine());
+        Console.Write("Введите конец отрезка: ");
+        var b = double.Parse(Console.ReadLine());
+        Console.Write("Введите точность вычислений: ");
+        var eps = double.Parse(Console.ReadLine());
+
+        var result = BisectionMethod(f, a, b, eps);
+        Console.WriteLine("x = {0}\r\nf({0}) = {1}", result, f(result));
+        Console.ReadKey();
+    }
+}*/
+        }
+
+        private void btn_sqr_Click(object sender, EventArgs e)
+        {
+            /*class Program
+{
+    static double LeftRectangle(Func<double, double> f, double a, double b, int n)
+    {
+        var h = (b - a) / n;
+        var sum = 0d;
+        for(var i = 0; i <= n-1; i++)
+        {
+            var x = a + i * h;
+            sum += f(x);
+        }
+
+        var result = h * sum;
+        return result;
+    }
+
+    static double RightRectangle(Func<double, double> f, double a, double b, int n)
+    {
+        var h = (b - a) / n;
+        var sum = 0d;
+        for (var i = 1; i <= n; i++)
+        {
+            var x = a + i * h;
+            sum += f(x);
+        }
+
+        var result = h * sum;
+        return result;
+    }
+
+    static double CentralRectangle(Func<double, double> f, double a, double b, int n)
+    {
+        var h = (b - a) / n;
+        var sum = (f(a) + f(b)) / 2;
+        for (var i = 1; i < n; i++)
+        {
+            var x = a + h * i;
+            sum += f(x);
+        }
+
+        var result = h * sum;
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        double f(double x) => x / (x - 1);
+
+        var result = LeftRectangle(f, 4, 10, 1000);
+        Console.WriteLine("Формула левых прямоугольников: {0}", result);
+        result = RightRectangle(f, 4, 10, 1000);
+        Console.WriteLine("Формула правых прямоугольников: {0}", result);
+        result = CentralRectangle(f, 4, 10, 1000);
+        Console.WriteLine("Формула средних прямоугольников: {0}", result);
+
+        Console.ReadKey();
+    }
+}*/
+        }
+
+        private void textSim_TextChanged(object sender, EventArgs e)
+        {
+            /**/
+        }
+
+        private void text_sqr_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text_del_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_sim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFormula_Click(object sender, EventArgs e)
+        {
+            string str = FormulaTextBox.Text;
+            string argS = ArgTextBox.Text;
+            FormulaPrep(ref str);
+            Formula f = CreateFormula(str);
+            float x = float.Parse(argS);
+            if (f != null)
+            {
+                resultTextBox.Text = f(x, 0, 0).ToString();
+            }
+            else
+            {
+                //resultTextBox.Text = str;
+                resultTextBox.Text = "Введена некорректная функция!!!";
+            }
+            
+        }
+    }
+}
